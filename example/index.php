b51fd3aba1a65bc958cbe5507468459a7b4178ef
arturiluck <arturiluck@gmail.com>
2020-05-01 23:35:14 +0300

    WEB-002_test_4

--- example/index.php
+++ example/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once "...../composer-package/vendor/autoload.php";
 
-//test 1 
+//test 1 2
 $factory = new \SharpMinds\Storage\Factory();
 //$factory->setCustomConfig($path);
 $factory->setConnectionParameters([
